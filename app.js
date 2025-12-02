eedd0309d834c5632c24d3d8c6cab9fcecadd983
Silver <saarsilver@hotmail.com>
2025-12-02 22:20:17 +0200

    fix: Correct Firebase initialization and service worker path

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// Outlive Tracker - app.js
+// Firebase App Initialization
+const firebaseConfig = {
+    apiKey: "AIzaSyAfJTkqlU9Y5yhn_g_HQXS9dNfOQTIQg3w",
+    authDomain: "habit-tracker-58ba9.firebaseapp.com",
+    projectId: "habit-tracker-58ba9",
+    storageBucket: "habit-tracker-58ba9.appspot.com",
+    messagingSenderId: "974837325994",
+    appId: "1:974837325994:web:ca99d56003fad33504b0d3"
+};
 
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+
+// Outlive Tracker - app.js
 const App = {
     user: null,
     userData: { profile: {}, logs: {} },
